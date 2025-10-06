052574bdc59a40214cbdbbb7bbeda6f4abb4b0a0
Damian Holona <damianholona074@gmail.com>
2025-10-06 21:38:28 +0200

    + homework - empty rectangle

---
+++ Task1/src/Task2/EmptyRectangle.java
@@ -0,0 +1,25 @@
+package Task2;
+
+import java.util.Scanner;
+
+public class EmptyRectangle {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Podaj wysokosc oraz szerokosc: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if (i==0 || j == 0 || i==a-1 || j == b - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
