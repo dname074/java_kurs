02f16448a1716316eed325022792e1a012daa7b8
Damian Holona <damianholona074@gmail.com>
2025-10-17 00:00:11 +0200

    + calculate PI task

---
+++ Homework3/src/Main.java
@@ -0,0 +1,62 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static List<Point> points = new ArrayList<>();
+    public static final Double R = 1.0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj ilosc losowych punktow: ");
+        int n = scanner.nextInt();
+
+        generatePoints(n);
+        int numbersInsideCircle = countPointsInCircle();
+
+        BigDecimal ratio = calculateRatio(numbersInsideCircle, n);
+
+        BigDecimal PI = getPI(ratio);
+        System.out.println("PI wynosi: " + PI);
+    }
+
+    private static void generatePoints(int n) {
+        Random random = new Random();
+
+        for (int i=0; i<n; i++) {
+            points.add(new Point(random.nextDouble(-R,R), random.nextDouble(-R,R)));
+        }
+    }
+
+    private static int countPointsInCircle() {
+        int i = 0;
+
+        for (Point point : points) {
+            if (isPointInCircle(point)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    private static boolean isPointInCircle(Point point) {
+        return getSquareOfNumber(point.getX()) + getSquareOfNumber(point.getY()) <= getSquareOfNumber(R);
+    }
+
+    private static Double getSquareOfNumber(Double number) {
+        return number*number;
+    }
+
+    private static BigDecimal calculateRatio(int nInCircle, int nTotal) {
+        double ratio = (double) nInCircle / nTotal;
+
+        return new BigDecimal(ratio);
+    }
+
+    private static BigDecimal getPI(BigDecimal ratio) {
+        return ratio.multiply(new BigDecimal(4));
+    }
+}
