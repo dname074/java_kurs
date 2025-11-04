db82e3a39d482f849b4fa8d084cee3ae777f32cf
Damian Holona <damianholona074@gmail.com>
2025-11-04 17:21:59 +0100

    + memoizacja ciag fibonacciego

--- Homework7/src/CiagFibonacciego.java
+++ Homework7/src/CiagFibonacciego.java
@@ -1,13 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CiagFibonacciego {
+    private static List<Long> sequence = new ArrayList<>();
+
+    static {
+        sequence.add(0L);
+        sequence.add(1L);
+    }
+
     public static void main(String[] args) {
-        System.out.println(count(48));
+        System.out.println(count(60));
     }
 
-    private static int count(int n) {
-        return countFibonacciSequence(n);
+    private static long count(int n) {
+        return countFibonacciSequenceImproved(n);
     }
 
-    private static int countFibonacciSequence(int n) {
+    private static long countFibonacciSequence(int n) {
         if (n == 0) {
             return 0;
         } else if (n==1) {
@@ -15,5 +25,12 @@ public class CiagFibonacciego {
         } else {
             return countFibonacciSequence(n-1) + countFibonacciSequence(n-2);
         }
+    }
+
+    private static long countFibonacciSequenceImproved(int n) {
+        if (sequence.size() <= n) {
+            sequence.add(countFibonacciSequenceImproved(n-1) + countFibonacciSequenceImproved(n-2));
+        }
+        return sequence.get(n);
     }
 }
