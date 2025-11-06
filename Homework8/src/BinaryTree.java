00bc3e372b6b48c5b201a0c88a66b8c549ae0f8a
Damian Holona <damianholona074@gmail.com>
2025-11-06 17:23:37 +0100

    + binaryTree iterate version

--- Homework8/src/BinaryTree.java
+++ Homework8/src/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class BinaryTree {
     private Node root;
 
@@ -34,6 +36,34 @@ public class BinaryTree {
         }
     }
 
+    public void insertByIteration(int key) {
+        if (root == null) {
+            root = new Node(key);
+            return;
+        }
+        Node temp = root;
+        while (true) {
+            if (root.getKey() == key) {
+                break;
+            }
+            if (temp.getKey() > key) {
+                if (temp.hasLeftChild()) {
+                    temp = temp.getLeftChild();
+                } else {
+                    temp.setLeftChild(new Node(key));
+                    break;
+                }
+            } else {
+                if (temp.hasRightChild()) {
+                    temp = temp.getRightChild();
+                } else {
+                    temp.setRightChild(new Node(key));
+                    break;
+                }
+            }
+        }
+    }
+
     public boolean contains(int key) {
         if (root == null) {
             return false;
@@ -60,15 +90,54 @@ public class BinaryTree {
         return false;
     }
 
+    public boolean containsKeyByIteration(int key) {
+        if (root == null) {
+            return false;
+        }
+
+        Node temp = root;
+        while (temp.getKey() != key) {
+            if (temp.getKey() > key) {
+                if (temp.hasLeftChild()) {
+                    temp = temp.getLeftChild();
+                } else {
+                    return false;
+                }
+            } else {
+                if (temp.hasRightChild()) {
+                    temp = temp.getRightChild();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public void print() {
         printTree(root);
     }
 
     private void printTree(Node node) {
-        if (node!=null) {
+        if (node != null) {
             printTree(node.getLeftChild());
             System.out.println(node.getKey());
             printTree(node.getRightChild());
+        }
+    }
+
+    public void inOrderIterate() {
+        Stack<Node> treeStack = new Stack<>();
+        Node temp = root;
+        while (temp != null || !treeStack.empty()) {
+            while (temp != null) {
+                treeStack.push(temp);
+                temp = temp.getLeftChild();
+            }
+            temp = treeStack.pop();
+            System.out.println(temp.getKey());
+
+            temp = temp.getRightChild();
         }
     }
 }
