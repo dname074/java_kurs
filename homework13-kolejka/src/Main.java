a30fe63496da10226d0dd39000476f8b2d821024
Damian H <damianholona074@gmail.com>
2025-11-16 15:08:17 +0100

    + queue task with locks and conditions

---
+++ homework13-kolejka/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.stream.IntStream;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) {
+        NewQueue queue = new NewQueue();
+        Runnable producer = () -> IntStream.range(1, 31).forEach(i -> {
+            try {
+                sleep(queue.random.nextInt(1000));
+                queue.produce();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Runnable consumer = () -> IntStream.range(1, 11).forEach(i -> {
+            try {
+                sleep(1500);
+                queue.consume();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread producer1 = new Thread(producer);
+        Thread consumer1 = new Thread(consumer);
+        Thread consumer2 = new Thread(consumer);
+        Thread consumer3 = new Thread(consumer);
+
+        producer1.start();
+        consumer1.start();
+        consumer2.start();
+        consumer3.start();
+    }
+}
+
