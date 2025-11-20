9d12536b6340fbf1b756b315557df17278019853
Damian Holona <damianholona074@gmail.com>
2025-11-20 15:36:47 +0100

    + executor array sum without list

--- homework15-arraysumExecutor/src/Main.java
+++ homework15-arraysumExecutor/src/Main.java
@@ -1,37 +1,29 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         long startTime = System.currentTimeMillis();
         ExecutorService executor = Executors.newFixedThreadPool(Constants.THREADS_AMOUNT);
         int totalSum = 0;
 
         int[] array = createAndFillArray();
         int partSize = Constants.ARRAY_SIZE / Constants.THREADS_AMOUNT;
-        List<Future<Integer>> results = new ArrayList<>();
 
         for (int i=0; i<Constants.THREADS_AMOUNT; i++) {
             int start = i * partSize;
             int end = (i == Constants.THREADS_AMOUNT - 1)? Constants.ARRAY_SIZE: start + partSize;
 
-            results.add(executor.submit(new Adder(start,end,array)));
+            totalSum += executor.submit(new Adder(start,end,array)).get();
         }
         executor.shutdown();
 
         try {
             executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
-            for (Future<Integer> result : results) {
-                totalSum += result.get();
-            }
-        } catch (InterruptedException | ExecutionException e) {
+        } catch (InterruptedException e) {
             System.err.println(e.getMessage());
         }
 
